593aa04443998afd433559394a3fe6049a9377a0
brycewoodland <bwoodlan@byui.edu>
2025-01-16 21:40:26 -0700

    Initial commit.

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {}
