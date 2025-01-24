918086390494c162d40671c40516dd21cb370428
brycewoodland <bwoodlan@byui.edu>
2025-01-24 11:59:55 -0700

    Completed assignment 3.

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
   standalone: false,
-  
   templateUrl: './message-edit.component.html',
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  @ViewChild('subject') subjectInputRef!: ElementRef;
+  @ViewChild('msgText') msgTextInputRef!: ElementRef;
 
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  currentSender: string = 'Bryce Woodland';
+
+  onSendMessage(): void{
+    const subject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgTextInputRef.nativeElement.value;
+
+    const newMessage = new Message(
+      '1', 
+      subject, 
+      msgText, 
+      this.currentSender
+    );
+  
+    this.addMessageEvent.emit(newMessage)
+    this.onClear();
+  }
+
+  onClear(): void {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages: Message[] = [];
 
+  ngOnInit(): void {
+    this.messages = [
+      new Message('1', 'Hello World', 'This is a test message.', 'Alice'),
+      new Message('2', 'Angular Rocks', 'Angular is a great framework for building web applications.', 'Bob'),
+      new Message('3', 'TypeScript', 'TypeScript adds static typing to JavaScript.', 'Charlie')
+    ];
+  }
+
+  onAddMessage(message: Message): void {
+    this.messages.push(message);
+  }
 }
