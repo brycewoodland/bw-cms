ccf536123af60271ef7af56f512194683d1c0313
brycewoodland <bwoodlan@byui.edu>
2025-01-29 11:22:08 -0700

    Implemented DocumentListComponent

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -9,21 +9,49 @@ import { Document } from '../document.model';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent implements OnInit {
-  documents: Document[] = [];
+  documents: Document[] = [
+    new Document(
+      '1', 
+      'CIT 260 - Object Oriented Programming', 
+      'Learn the fundamentals of OOP.', 
+      'http://example.com/doc1'
+    ),
+    new Document(
+      '2', 
+      'CIT 366 - Full Web Stack Development', 
+      'Learn how to develop modern web applications using the MEAN stack.', 
+      'http://example.com/doc2'
+    ),
+    new Document(
+      '3', 
+      'CIT 425 - Data Warehousing', 
+      'Learn how to design, secure, and maintain databases.', 
+      'http://example.com/doc3'
+    ),
+    new Document(
+      '4', 
+      'CIT 460 - Enterprise Development', 
+      'Learn how to develop enterprise business applications.', 
+      'http://example.com/doc4'
+    ),
+    new Document(
+      '5', 
+      'CIT 495 - Senior Practicum', 
+      'Design a senior project that illustrates skills you have learned.', 
+      'http://example.com/doc5'
+    )
+  ];
 
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  ngOnInit(): void {
-    this.documents = [
-      new Document('1', 'CIT 260', 'Object Oriented Programming', 'http://example.com/doc1'),
-      new Document('2', 'CIT 366', 'Full Web Stack Development', 'http://example.com/doc2'),
-      new Document('3', 'CIT 425', 'Data Warehousing', 'http://example.com/doc3'),
-      new Document('4', 'CIT 460', 'Enterprise Development', 'http://example.com/doc4'),
-      new Document('5', 'CIT 495', 'Senior Practicum', 'http://example.com/doc5')
-    ];
-  }
-
-  onSelectedDocument(document: Document): void {
+  onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
   }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
 }
