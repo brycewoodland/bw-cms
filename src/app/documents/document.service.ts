ecbfc2fe23a2a15f728d1b6467ad2ded6857a4b6
brycewoodland <bwoodlan@byui.edu>
2025-02-07 15:06:00 -0700

    Implement Document Service.

---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Document } from '../documents/document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  documents: Document [] = [];
+  documentSelectedEvent = new EventEmitter<Document>();
+
+  constructor() { 
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments() {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document {
+    for (let document of this.documents) {
+      if (document.id === id) {
+        return document;
+      }
+    }
+
+    return null;
+  }
+}
