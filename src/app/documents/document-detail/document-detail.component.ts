19137d861654a74625a931b966a1f119375d16bf
brycewoodland <bwoodlan@byui.edu>
2025-02-12 17:11:56 -0700

    Finished implementing the document detail component route.

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-document-detail',
   standalone: false,
-  
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css'
 })
-export class DocumentDetailComponent {
-  @Input() document: Document;
-}
+
+export class DocumentDetailComponent implements OnInit {
+  document: Document;
+
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.document = this.documentService.getDocument(id);
+    })
+  }
+} 
