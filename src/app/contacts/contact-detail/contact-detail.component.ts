1c8b6129e7e2a1fa41da41ec88fe048a4b8cf003
brycewoodland <bwoodlan@byui.edu>
2025-02-14 19:26:15 -0700

    Directive to Load the ContactDetailComponent.

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -7,6 +9,19 @@ import { Contact } from '../contact.model';
   templateUrl: './contact-detail.component.html',
   styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
+export class ContactDetailComponent implements OnInit {
   @Input() contact: Contact;
+
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.contact = this.contactService.getContact(id);
+    })
+  }
 }
