c7643c4f9b8760b5e8f4191773f211f932ed8abc
brycewoodland <bwoodlan@byui.edu>
2025-02-27 19:37:38 -0700

    Added add contact form functionality.

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-contact-edit',
   standalone: false,
-  
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
 export class ContactEditComponent {
+  contact: Contact;
+  groupContacts: Contact[];
 
+
+  onSubmit(form: NgForm) {
+
+  }
+
+  onCancel() {
+
+  }
 }
